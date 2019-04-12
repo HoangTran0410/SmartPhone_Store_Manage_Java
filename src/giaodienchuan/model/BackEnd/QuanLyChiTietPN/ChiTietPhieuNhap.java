208d09b0312fad11ca8a8315a72d0ffbaa8fa2cf
thanhgianga303 <45537576+thanhgianga303@users.noreply.github.com>
2019-04-12 11:21:22 +0700

    Update ChiTietPhieuNhap.java

--- src/giaodienchuan/model/BackEnd/QuanLyChiTietPN/ChiTietPhieuNhap.java
+++ src/giaodienchuan/model/BackEnd/QuanLyChiTietPN/ChiTietPhieuNhap.java
@@ -10,5 +10,16 @@ package giaodienchuan.model.BackEnd.QuanLyChiTietPN;
  * @author Admin
  */
 public class ChiTietPhieuNhap {
+    String ma;
+    String maSP;
+    Integer soLuong;
+    Integer donGia;
+    public ChiTietPhieuNhap(){
+        
+    }
+    
+    public ChiTietPhieuNhap(String ma,String maSP,String soLuong, String donGia){
+    this.ma-m    
+        }
     
 }
