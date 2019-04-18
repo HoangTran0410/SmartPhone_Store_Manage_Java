f252aed6713c889dac3dd99ede1665e15b674df6
Hoang Tran <th01634044850@gmail.com>
2019-04-18 23:09:24 +0700

    new lookAndFeel

--- src/giaodienchuan/Main.java
+++ src/giaodienchuan/Main.java
@@ -1,10 +1,17 @@
 package giaodienchuan;
 
 import giaodienchuan.model.FrontEnd.GiaoDienChuan.GiaoDienChuan;
+import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+
+        }
+
         GiaoDienChuan f = new GiaoDienChuan();
         f.setVisible(true);
     }
