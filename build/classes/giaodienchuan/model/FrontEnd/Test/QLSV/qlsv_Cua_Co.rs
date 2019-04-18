84b8d613ea15eea96a5fea57522c1c5d443cc0c2
thanhgianga303 <45537576+thanhgianga303@users.noreply.github.com>
2019-04-18 21:31:52 +0700

    delete build

--- build/classes/giaodienchuan/model/FrontEnd/Test/QLSV/qlsv_Cua_Co.rs
+++
@@ -1,4 +0,0 @@
-giaodienchuan.model.FrontEnd.Test.QLSV.SinhVienDTO
-giaodienchuan.model.FrontEnd.Test.QLSV.SinhVienDAO
-giaodienchuan.model.FrontEnd.Test.QLSV.SinhVienBUS
-giaodienchuan.model.FrontEnd.Test.QLSV.SinhVienForm
