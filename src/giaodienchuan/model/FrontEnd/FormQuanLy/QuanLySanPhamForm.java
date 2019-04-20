8fad50f7b314c712a03109de49f8412430deca07
Hoang Tran <th01634044850@gmail.com>
2019-04-20 18:36:57 +0700

    remove refresh after sua san pham

--- src/giaodienchuan/model/FrontEnd/FormQuanLy/QuanLySanPhamForm.java
+++ src/giaodienchuan/model/FrontEnd/FormQuanLy/QuanLySanPhamForm.java
@@ -43,7 +43,6 @@ public class QuanLySanPhamForm extends JPanel {
         });
         btnSua.addActionListener((ActionEvent ae) -> {
             btnSuaMouseClicked();
-            formHienThi.refresh();
         });
     }
 
