3f03ac01e9b303d97320b580bffea015e40d8380
Hoang Tran <th01634044850@gmail.com>
2019-04-23 01:40:46 +0700

    login form

--- src/giaodienchuan/Main.java
+++ src/giaodienchuan/Main.java
@@ -1,18 +1,18 @@
 package giaodienchuan;
 
-import giaodienchuan.model.FrontEnd.GiaoDienChuan.GiaoDienChuan;
+import giaodienchuan.model.FrontEnd.GiaoDienChuan.LoginForm;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
 
         }
-
-        GiaoDienChuan f = new GiaoDienChuan();
-        f.setVisible(true);
+        
+        new LoginForm().setVisible(true);
     }
 }
